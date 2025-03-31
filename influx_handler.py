2d0bcd31810377be0b9b4f3d10485fe7a4e4ad6c
Shivam Bhagat <109543410+shivbhagat03@users.noreply.github.com>
2025-03-31 12:52:03 +0530

    influx_handler.py

--- influx_handler.py
+++ influx_handler.py
@@ -3,10 +3,8 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 import datetime
 from config import INFLUXDB_TOKEN, INFLUX_URL, INFLUX_ORG, INFLUX_BUCKET
 
-
 class InfluxHandler:
     def __init__(self, url="INFLUX_URL", token="INFLUXDB_TOKEN", org="INFLUX_ORG", bucket="INFLUX_BUCKET"):
-        """Initialize InfluxDB connection"""
         self.client = InfluxDBClient(url=url, token=token, org=org)
         self.write_api = self.client.write_api(write_options=SYNCHRONOUS)
         self.bucket = bucket
@@ -21,17 +19,16 @@ class InfluxHandler:
     def write_data(self, measurement, machineId, stroke_count, timestamp):
         try:
             
-            time_obj = datetime.datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S.%fZ")
-            
-            
+            ist_time = datetime.datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S.%fZ").replace(tzinfo=datetime.timezone(datetime.timedelta(hours=5, minutes=30)))
+            utc_time = ist_time.astimezone(datetime.timezone.utc)
             point = Point(measurement) \
                 .tag("machineId", machineId) \
                 .field("totalStrokeCounter", stroke_count) \
-                .time(time_obj)
+                .time(utc_time)
 
             
             self.write_api.write(bucket=self.bucket, org=self.org, record=point)
-            print(f"Successfully wrote data point: Machine ID={machineId}, Stroke Count={stroke_count}, Time={timestamp}")
+            print(f"Successfully wrote data point: Machine ID={machineId}, Stroke Count={stroke_count}, Time={utc_time}")
             
             
             query = f'''
